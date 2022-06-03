968c5b3e98bd6708feb48fa9433122e33ed52b93
Santiago Estévez <sestevez5@gmail.com>
2022-06-03 21:36:33 +0100

    Creado el componente login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { GestionDtosComponent } from './componentes/dtos/gestion-dtos/gestion-dtos.component';
-import { ListaDtosComponent } from './componentes/dtos/lista-dtos/lista-dtos.component';
-import { DetalleDtoComponent } from './componentes/dtos/detalle-dto/detalle-dto.component';
-import { DetalleEndpointComponent } from './componentes/endpoints/detalle-endpoint/detalle-endpoint.component';
-import { ListaEndpointsComponent } from './componentes/endpoints/lista-endpoints/lista-endpoints.component';
-import { GestionEndpointsComponent } from './componentes/endpoints/gestion-endpoints/gestion-endpoints.component';
+
+// Angular Material
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -17,6 +13,18 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
+// Componentes
+import { GestionDtosComponent } from './componentes/dtos/gestion-dtos/gestion-dtos.component';
+import { ListaDtosComponent } from './componentes/dtos/lista-dtos/lista-dtos.component';
+import { DetalleDtoComponent } from './componentes/dtos/detalle-dto/detalle-dto.component';
+import { DetalleEndpointComponent } from './componentes/endpoints/detalle-endpoint/detalle-endpoint.component';
+import { ListaEndpointsComponent } from './componentes/endpoints/lista-endpoints/lista-endpoints.component';
+import { GestionEndpointsComponent } from './componentes/endpoints/gestion-endpoints/gestion-endpoints.component';
+import { LoginComponent } from './componentes/general/login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +34,10 @@ import { MatExpansionModule } from '@angular/material/expansion';
     DetalleDtoComponent,
     DetalleEndpointComponent,
     ListaEndpointsComponent,
-    GestionEndpointsComponent
+    GestionEndpointsComponent,
+    LoginComponent,
+
+
   ],
   imports: [
     BrowserModule,
@@ -39,6 +50,9 @@ import { MatExpansionModule } from '@angular/material/expansion';
     MatListModule,
     MatButtonModule,
     MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
