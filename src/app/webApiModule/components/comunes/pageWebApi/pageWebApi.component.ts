7fd237af6fc2ec37006f2b56986edd7f654d1588
Santiago Estévez <sestevez5@gmail.com>
2022-07-03 09:28:35 +0100

    Montando el selector de entornos

--- src/app/webApiModule/components/comunes/pageWebApi/pageWebApi.component.ts
+++ src/app/webApiModule/components/comunes/pageWebApi/pageWebApi.component.ts
@@ -1,10 +1,14 @@
+import { WebapiService } from './../../../services/webapi.service';
 
 import { IDto } from '../../../models/dtoModel';
 
 import { Component, Input, OnInit } from '@angular/core';
 
 
-
+interface Food {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-pageWebApi',
@@ -12,4 +16,20 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./pageWebApi.component.css']
 })
 export class PageWebApiComponent {
+
+  constructor(was: WebapiService){
+    was.obtenerDocumentos()
+  }
+
+  documentosOpenApiPrefijados: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
+
+  selectedFood = this.foods[2].value;
+
+  selectCar(event: Event) {
+    this.selectedFood = (event.target as HTMLSelectElement).value;
+  }
 }
